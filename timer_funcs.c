939d670e38d6854230a6cef5e6467d02859bc3de
ekrov <carlos.antunes.almeida@gmail.com>
2024-01-07 23:32:15 +0000

    timer update

--- timer_funcs.c
+++ timer_funcs.c
@@ -2,7 +2,7 @@
  * timer_funcs.c
  *
  *  Created on: 29/12/2023
- *      Author: carlo
+ *      Author: carlos
  */
 
 #include "timer_funcs.h"
@@ -15,27 +15,27 @@ uint32_t counter = 0;
 
 // Initialize Timer0A
 void Timer0A_Init(void) {
-    // Habilita o periférico do Timer 0
+    // Enable of periferic of timer 0
     SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
 
-    // Configura o Timer 0 para operar em modo periódico (up-count) e habilita o timer
+    // Configures timer 0 to operate in periodic more, and enables the timer itself
     TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);
     TimerEnable(TIMER0_BASE, TIMER_A);
 
-    // Define o valor de recarga do Timer 0 para 10 segundo
+    // Defines the recharge of timer 0 to 10 seconds
     TimerLoadSet(TIMER0_BASE, TIMER_A, SysCtlClockGet());
 
-    // Registra a função de tratamento de interrupção para o Timer 0
+    // Registers the interrupt handler of timer 0
     TimerIntRegister(TIMER0_BASE, TIMER_A, timer_count_time);
 
-    // Habilita a interrupção de estouro do Timer 0
+    // Enable of itnerrupt of timer 0
     TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
 
-    // Habilita as interrupções globalmente
+    // Interrupt global enable
     IntMasterEnable();
 
 }
-//Função que adiciona um segundo à contagem
+//Function that adds a second to the counter and passes it on to a queue
 void timer_count_time(void)
 {
     TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
